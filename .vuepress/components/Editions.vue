65b777a6cff16cae2b74b4ac3c66a9dd43e4ff00
Fran Mosteiro <fran.mosteiro@sequra.es>
2022-10-24 13:09:22 +0200

    Add previous editions form archive.org

--- .vuepress/components/Editions.vue
+++ .vuepress/components/Editions.vue
@@ -1,10 +1,26 @@
 <template>
     <div class="past-events">
         <p>
-            Check previous editions: 
+            Check previous editions <br />
+            <a href="https://web.archive.org/web/20120502190422/http://bilbostack.com/" target="_blank"><b>2012</b></a> 
+            | 
+            <a href="https://web.archive.org/web/20130110075437/http://bilbostack.com/" target="_blank"><b>2013</b></a> 
+            | 
+            <a href="https://web.archive.org/web/20140205142714/http://bilbostack.com/" target="_blank"><b>2014</b></a> 
+            | 
+            <a href="https://web.archive.org/web/20150109210526/http://bilbostack.com/" target="_blank"><b>2015</b></a> 
+            | 
+            <a href="https://web.archive.org/web/20160109081707/http://bilbostack.com/" target="_blank"><b>2016</b></a> 
+            | 
+            <a href="https://web.archive.org/web/20170601095000/http://bilbostack.com/" target="_blank"><b>2017</b></a> 
+            | 
+            <a href="https://web.archive.org/web/20180129112902/http://bilbostack.com/" target="_blank"><b>2018</b></a> 
+            | 
             <a href="https://2019.bilbostack.com" target="_blank"><b>2019</b></a> 
             | 
             <a href="https://2020.bilbostack.com" target="_blank"><b>2020</b></a>
+            <br />| 
+            <span><b>2😷21</b></span>
             | 
             <a href="https://2022.bilbostack.com" target="_blank"><b>2022</b></a>
         </p>
